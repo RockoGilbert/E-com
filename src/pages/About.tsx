ca3f0d4d97e01ea53e2751aa29cde52031f9d4a5
RockoGilbert <evolve2122@gmail.com>
2023-04-28 04:37:38 -0500

    Edited Links for Home and About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,15 @@
-export function About() {
-    return <h1>About</h1>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './About.css';
+
+export default function About() {
+    return (
+        <div className="about">
+            <h1>About Our Business</h1>
+            <p>We are an online wholesaling/dropshipping business that sells a variety of products sourced from around the world. Our mission is to provide high-quality products at competitive prices to retailers and consumers alike.</p>
+            <p>If you are interested in buying our products, please visit our <Link to="/store">store</Link> or <Link to="/">homepage</Link>.</p>
+        </div>
+    );
 }
+
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
-export function Home() {
-    return
-    <div>
-        <h1>Home</h1>
-        <p>Proceed to Store</p>
-    </div>
+import React from "react";
+import { Store } from './Store'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
+
+export function Home() {
+    return (
+        <div className="home">
+            <h1>Welcome to our online store</h1>
+            <p>We sell a variety of unique items that we've found in our travels.</p>
+            <Link to="store">
+                <button onClick={Store}>Shop Now</button>
+            </Link>
+        </div>
+    );
 }
